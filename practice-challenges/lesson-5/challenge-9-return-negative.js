f7559f7242dd58172f5222ced5a4c8e711aa346d
virgileblais <virgile.blais@gmail.com>
2023-08-05 23:38:55 -0400

    Revised Challenge 9-Return Negative

--- practice-challenges/lesson-5/challenge-9-return-negative.js
+++ practice-challenges/lesson-5/challenge-9-return-negative.js
@@ -21,10 +21,22 @@ function makeNegative(num) {
 /*
 REVISED ANSWER:
 The function failed to pass the Code Wars test as it was not returning a value to evaluate via the test. I thus added a 'return' statement.
-*/
 
 function makeNegative(num) {
     return (-(num));
   }
+*/
 
-//ANSWER is CORRECT
+//ANSWER is INCORRECT
+
+/*
+REVISED ANSWER 2:
+Prior code passed Code Wars test but not the attempt evaluation.
+I realized that I had partially misunderstood that the question requested for negative inputs to remain negative, however I had previously interpreted that as a request for negative inputs to be made positive.
+*/
+
+function makeNegative(num) {
+    return -Math.abs(num);
+  }
+
+//ANSWER is INCORRECT
