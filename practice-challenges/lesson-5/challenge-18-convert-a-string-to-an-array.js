7e6fe5cbce6e8f66cc1129249a37db40694fe152
virgileblais <virgile.blais@gmail.com>
2023-08-13 14:21:43 -0400

    Completed Challenge 18- Convert a string to an array

---
+++ practice-challenges/lesson-5/challenge-18-convert-a-string-to-an-array.js
@@ -0,0 +1,16 @@
+/*
+CHALLENGE:
+Write a function to split a string and convert it into an array of words.
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+
+/*
+ANSWER:
+
+*/
+
+function stringToArray(string){
+    return string.split(' ');
+  }
+
+//ANSWER is CORRECT
