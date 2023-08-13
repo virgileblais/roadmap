1d2a9de580a38dc11dc14651281419990a9e94aa
virgileblais <virgile.blais@gmail.com>
2023-08-13 14:28:20 -0400

    Completed Challenge 19- Remove string spaces

---
+++ practice-challenges/lesson-5/challenge-19-remove-string-spaces.js
@@ -0,0 +1,15 @@
+/*
+CHALLENGE:
+Write a function that removes the spaces from the string, then return the resultant string.
+*/
+
+/*
+ANSWER:
+
+*/
+
+function noSpace(x) {
+    return x.replaceAll(' ', '');
+  }
+
+//ANSWER is CORRECT
