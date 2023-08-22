e2c53160b6f6b6a83e707dd0bdc0cdd602eaeeb6
virgileblais <virgile.blais@gmail.com>
2023-08-22 18:08:46 -0400

    Lesson 10- Challenge 1: Added start files

---
+++ code-challenges/lesson-10/challenge-1-lesson-8-redo-with-flexbox/script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("button")[0].onclick = () => {
+    alert("Thanks for signing-up! Your saved credit card will be automatically charged monthly on your payment date.")
+    alert("Just kidding :)");
+  }
